8b1d30af0063aecb1b7d1f62a92da6a6dedf27f2
iNorthIsle <ndobox@outlook.com>
2022-09-13 19:32:48 +0800

    feat: convert hex value to decimal

--- pages/api/v1/swap/calculateFee.js
+++ pages/api/v1/swap/calculateFee.js
@@ -1,6 +1,10 @@
 import mesonPresets from '@mesonfi/presets'
-import { BigNumber } from 'ethers'
+import { BigNumber, ethers } from 'ethers'
 import { Rules, FeeWaives } from 'lib/db'
+
+const fromValue = (value) => {
+  return ethers.utils.formatUnits(value || '0', 6).replace(/\.0*$/, '')
+}
 
 const _matchNetworkToken = (rule, [n1, t1]) => {
   let n0, t0
@@ -34,13 +38,13 @@ const matchSwapRule = (rules, { from, to }) => {
 
 async function getSwapRules(initiator) {
   const rules = await Rules.find().sort({ priority: -1 }).exec()
-    if (initiator) {
-      const date = new Date()
-      date.setUTCHours(0,0,0,0)
-      const waived = await FeeWaives.findById(`${initiator.toLowerCase()}:${date.valueOf() / 1000}`)
-      return { rules, waived: waived?.waived || 0, swaps: waived?.swaps || 0 }
-    }
-    return { rules }
+  if (initiator) {
+    const date = new Date()
+    date.setUTCHours(0, 0, 0, 0)
+    const waived = await FeeWaives.findById(`${initiator.toLowerCase()}:${date.valueOf() / 1000}`)
+    return { rules, waived: waived?.waived || 0, swaps: waived?.swaps || 0 }
+  }
+  return { rules }
 }
 
 export default async function handler(req, res) {
@@ -48,28 +52,41 @@ export default async function handler(req, res) {
     // NOTE: non-preminum address, just using for data query
     const address = '0x18D594bF5213A847c001775d8C4AaC9427284774'
     const { token, inChain, outChain, amount: queryAmount } = req.query
+    const tokenIndex = token === 'USDC' ? 1 : token === 'USDT' ? 2 : null
+
+    if (queryAmount > 5000) {
+      res.json({
+        message: 'The amount cannot exceed 5000',
+        code: 500
+      })
+    }
+
+    if (!tokenIndex) {
+      res.json({
+        message: 'The swap route must has specific token.',
+        code: 500
+      })
+    }
 
     const rulesInfo = await getSwapRules(address)
 
-
-    await getSwapRules()
     const networks = mesonPresets.getAllNetworks()
 
     const fromNetwork = networks.find(item => item.name === inChain)
     const toNetwork = networks.find(item => item.name === outChain)
+    const fromSymbol = fromNetwork.tokens.find(item => item.symbol.startsWith(token)).symbol
+    const toSymbol = toNetwork.tokens.find(item => item.symbol.startsWith(token)).symbol
 
     const rule = matchSwapRule(rulesInfo.rules, {
-      from: [fromNetwork.id, token],
-      to: [toNetwork.id, token]
+      from: [fromNetwork.id, fromSymbol],
+      to: [toNetwork.id, toSymbol]
     })
-
     if (rule.limit === 0) {
       res.json({
         message: 'This swap route is not available',
         code: 500
       })
     }
-
     const amount = BigNumber.from(queryAmount || '0')
     // const waiveServiceFee = swapData.premium
     // ? swapData.premium.used < swapData.premium.quota
@@ -83,7 +100,13 @@ export default async function handler(req, res) {
 
     if (!rule || amount.eq(0)) {
       res.json({
-        totalFee, originalFee, lpFee, waiveServiceFee, waived: rulesInfo.waived,
+        data: {
+          totalFee,
+          originalFee,
+          lpFee,
+          waiveServiceFee,
+          waived: rulesInfo.waived,
+        },
         code: 200
       })
     }
@@ -106,7 +129,14 @@ export default async function handler(req, res) {
     }
 
     res.json({
-      data: { totalFee, originalFee, lpFee, waiveServiceFee, waived: rulesInfo.waived, swaps: rulesInfo.swaps },
+      data: {
+        totalFee: fromValue(totalFee),
+        originalFee: fromValue(originalFee),
+        lpFee: fromValue(lpFee),
+        waiveServiceFee,
+        waived: rulesInfo.waived,
+        swaps: rulesInfo.swaps
+      },
       code: 200
     })
   } else {
