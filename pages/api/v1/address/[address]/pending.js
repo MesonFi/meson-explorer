9ebb1344ff31986f9a34a456f33f66520a52ee6d
Phil <qft.gtr@gmail.com>
2024-03-19 17:54:26 +0800

    Update pending.js

--- pages/api/v1/address/[address]/pending.js
+++ pages/api/v1/address/[address]/pending.js
@@ -3,22 +3,23 @@ import { SWAP_RES_FIELDS } from 'lib/const'
 
 export default async function handler(req, res) {
   const address = req.query.address
-  const since = Date.now() - 3600_000 * 6
+  const createdSince = Date.now() - 3600_000 * 24 * 7
+  const updatedSince = Date.now() - 3600_000 * 6
   const query = {
-    'fromTo.0': address,
-    updated: { $gt: since },
-    'events.name': { $eq: 'BONDED', $nin: ['RELEASED', 'CANCELLED'] },
-    // $or: [
-    //   { updated: { $gt: since } },
-    //   { 'events.name': { $eq: 'BONDED', $nin: ['RELEASED', 'CANCELLED'] } },
-    // ],
+    fromTo: address,
+    created: { $gt: createdSince },
+    $or: [
+      { updated: { $gt: updatedSince } },
+      { 'events.name': { $eq: 'BONDED', $nin: ['RELEASED', 'CANCELLED'] } },
+    ],
     disabled: { $exists: false }
   }
   
-  const list = await Swaps.find(query)
+  const result = await Swaps.find(query)
     .select(SWAP_RES_FIELDS)
     .sort({ created: -1 })
     .exec()
 
+  const list = result.filter(item => item.fromTo[0] === address)
   res.json({ result: { total: list.length, list } })
 }
